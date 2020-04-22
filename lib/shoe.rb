cee8e6e9f91b11ca080618dd74874fdcb86997a4
tiya adugna <tiyakefalee@gmail.com>
2020-04-22 07:28:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 # Make your shoe class here!
 
 class Shoe 
+  attr_accessor :brand, :size, :material, :condition 
   
 def initialize(style)
   @style = style
 end 
+def cobble
+  puts ""
+end
 end
   
